3b72029fea3863c9479a778d546e32398c3208b8
c++ <fattohabdallah2@gmail.com>
2025-02-17 13:10:39 +0300

    Add files via upload

---
+++ app.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+
+  void  calc ( )
+  {
+  int a = 1;  
+    if ( a == 1)
+    {
+      std ::cout <<"Don,t loser\n";
+    } 
+  }
+                                                                    //end
+
+    void  calc2 ( )
+    {
+    int b = 2;  
+      if ( b == 2)
+      {
+        std ::cout <<"I mise you\n";
+      }
+    }
+                                                                //end
+
+                int main ()
+              {
+                
+                int s ;
+              std ::cin >> s;
+              if ( s == 1 )
+              {
+                calc ();
+              }
+                                                        //bace spese
+
+      int m ;
+      std ::cin >> m;
+      if ( m == 2 )
+      {
+        calc2 ();
+      }
+
+                       return 0;
+}
+
+                  //تمت كتابة هذا الكود في 2025\2\17  \\         
